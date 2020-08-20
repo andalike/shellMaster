22cc916390964ec4a49d501e798ad5573bf6e255
Ankit <43137695+andalike@users.noreply.github.com>
2020-08-20 07:37:58 +0530

    Update a1.sh

--- a1.sh
+++ a1.sh
@@ -4,8 +4,10 @@ echo 'Step 102:Installing/Updating apache2'
 sudo apt-get install -y apache2
 cd /home/ubuntu
 rm -rf personalWebSite/
+echo 'Step 103:Cloning the repo'
 git clone https://github.com/andalike/personalWebSite.git
+echo 'Step 104:Repo Cloned'
 cd personalWebSite
-echo 'Step 103:Copying the source files'
+echo 'Step 105:Copying the source files'
 sudo cp -r *  /var/www/html
-echo 'Step 104:Source code Deployed'
+echo 'Step 106:Source code Deployed'
